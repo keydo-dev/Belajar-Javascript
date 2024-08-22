5725caf0263ce54bdaf775e36c42ab9399ddda45
keydo-dev <syafrilkeydo2009@gmail.com>
2024-08-22 09:34:02 +0700

    keydo

---
+++ variable.js
@@ -0,0 +1,30 @@
+// Ini Contoh Komentar
+
+/*
+Ini juga Komentar
+berbaris
+*/
+
+// Deklarasi
+let variable
+
+// Inisiasi
+let variable2 = 12422;
+
+/*
+LET
+1. Bisa Di ganti valuenya
+2. Tidak Bsa Dideklarasi ulang
+*/
+
+let asd = "ABC";
+asd = 123;
+
+/*
+CONST
+1. Value Tetap / tidak bisa di ganti
+2. tidak bisa dideklarasi ulang
+*/
+const angka = 123;
+const angka2 = angka + 5
+console.log(angka)
